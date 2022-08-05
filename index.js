7e1b99c91618445ac42eca417fd0cd916e0dded8
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-05 02:35:35 -0600

    Added background information

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Initialization of OpenStreetMap
-const map = L.map("leaflet-map").setView([35.08449, -106.65114], 13);
+const map = L.map("leaflet-map").setView([50.4501, 30.5234], 13);
 const openStreetMap = L.tileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   {
